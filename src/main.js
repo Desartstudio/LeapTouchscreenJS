061465d62b9e415df2950724a94260b694c54ab3
Jared Deckard <deckar01@nsuok.edu>
2013-01-19 13:59:04 -0600

    Improved calibration, shortcut to youtube demo

--- src/main.js
+++ src/main.js
@@ -19,32 +19,50 @@ var Touchscreen = (function () {
 	}
 	
 	module.output = null;
-	module.plane = null;
-	module.currentFrame = {};	
-	module.capturedPoints = [];
+	module.plane = null;	
+	module.normals = [];
+	
+	module.calibrate1 = function() {
+		var point = module.controller.frame().pointables()[0].tipPosition();
+		module.normals[0] = point;
+		module.output.append(point + '<br>');
+		document.getElementById('point1').style.visibility = 'hidden';
+		document.getElementById('point2').style.visibility = 'visible';
+		$('#calibrate').off('click');
+		$('#calibrate').on('click', module.calibrate2);
+	}
+	
+	module.calibrate2 = function() {
+		var point = module.controller.frame().pointables()[0].tipPosition();
+		module.normals[1] = point;
+		module.output.append(point + '<br>');
+		document.getElementById('point2').style.visibility = 'hidden';
+		document.getElementById('point3').style.visibility = 'visible';
+		$('#calibrate').off('click');
+		$('#calibrate').on('click', module.calibrate3);
+	}
+	
+	module.calibrate3 = function() {
+		var point = module.controller.frame().pointables()[0].tipPosition();
+		module.normals[2] = point;
+		module.output.append(point + '<br>');
+		document.getElementById('point3').style.visibility = 'hidden';
+		$('#calibrate').off('click');
+		module.initPlane();
+	}
 
 	module.initView = function () {
+	
 		this.output = $('#output');
-
-		$('#calibrate').on('click', function() {
-			Touchscreen.capturedPoints.length = 0;
-			module.output.empty();
-			setTimeout(captureCalibratePoint, 1500);
-			setTimeout(captureCalibratePoint, 2700);
-			setTimeout(captureCalibratePoint, 4300);
+		$('#calibrate').on('click', function(){
+			document.getElementById('point1').style.visibility = 'visible';
+			$('#calibrate').on('click', module.calibrate1);
 		});
-	
-		function captureCalibratePoint() {
-			var point = module.controller.frame().pointables()[0].tipPosition();
-			module.capturedPoints.push(point);
-			module.output.append(point + '<br>');
-			if (module.capturedPoints.length > 2) module.initPlane();
-		}
-	
 	};
 
 	module.initPlane = function() {
-		this.plane = new Plane(this.capturedPoints[0],this.capturedPoints[1],this.capturedPoints[2]);
+		
+		this.plane = new Plane(this.normals[0],this.normals[1],this.normals[2]);
 		
 		this.listener.onFrame = function(controller){
 			var pointableList = controller.frame().pointables();
@@ -53,14 +71,14 @@ var Touchscreen = (function () {
 			for(index = 0; index < pointableList.count(); index++){
 			
 				var pointable = pointableList[index];
-				var hit = Touchscreen.plane.rayIntersect(pointable.tipPosition(), pointable.direction());
+				var hit = Touchscreen.plane.pointIntersect(pointable.tipPosition());
 	
-				if (hit && hit.distance < 2) {
+				if (hit && hit.distance < 10) {
 					isHit = true;
 				}
 			}
 			
-			console.log(isHit);
+			//console.log(isHit);
 		
 			if (isHit) {
 				$('body').css('background-color','#ccc');
