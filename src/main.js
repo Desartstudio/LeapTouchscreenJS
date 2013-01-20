3bc0df51609492bc899975ab07abe92040f9253a
Jared Deckard <deckar01@nsuok.edu>
2013-01-19 22:10:23 -0600

    Translate plane intersect to screen coordinates

--- src/main.js
+++ src/main.js
@@ -20,49 +20,75 @@ var Touchscreen = (function () {
 	
 	module.output = null;
 	module.plane = null;	
-	module.normals = [];
+	module.points = [];
 	
 	module.calibrate1 = function() {
-		var point = module.controller.frame().pointables()[0].tipPosition();
-		module.normals[0] = point;
-		module.output.append(point + '<br>');
-		document.getElementById('point1').style.visibility = 'hidden';
-		document.getElementById('point2').style.visibility = 'visible';
-		$('#calibrate').off('click');
-		$('#calibrate').on('click', module.calibrate2);
+		var p = module.controller.frame().pointables();
+		if(p.count()==1){
+			var point = p[0].tipPosition();
+			module.points[0] = point;
+			module.output.append(point + '<br>');
+			$('#point1')[0].style.visibility = 'hidden';
+			$('#point2')[0].style.visibility = 'visible';
+			$('#calibrate').off('click');
+			$('#calibrate').on('click', module.calibrate2);
+		}
 	}
 	
 	module.calibrate2 = function() {
-		var point = module.controller.frame().pointables()[0].tipPosition();
-		module.normals[1] = point;
-		module.output.append(point + '<br>');
-		document.getElementById('point2').style.visibility = 'hidden';
-		document.getElementById('point3').style.visibility = 'visible';
-		$('#calibrate').off('click');
-		$('#calibrate').on('click', module.calibrate3);
+		var p = module.controller.frame().pointables();
+		if(p.count()==1){
+			var point = p[0].tipPosition();
+			module.points[1] = point;
+			module.output.append(point + '<br>');
+			$('#point2')[0].style.visibility = 'hidden';
+			$('#point3')[0].style.visibility = 'visible';
+			$('#calibrate').off('click');
+			$('#calibrate').on('click', module.calibrate3);
+		}
 	}
 	
 	module.calibrate3 = function() {
-		var point = module.controller.frame().pointables()[0].tipPosition();
-		module.normals[2] = point;
-		module.output.append(point + '<br>');
-		document.getElementById('point3').style.visibility = 'hidden';
-		$('#calibrate').off('click');
-		module.initPlane();
+		var p = module.controller.frame().pointables();
+		if(p.count()==1){
+			var point = p[0].tipPosition();
+			module.points[2] = point;
+			module.output.append(point + '<br>');
+			$('#point3')[0].style.visibility = 'hidden';
+			$('#calibrate').off('click');
+			module.initPlane();
+		}
 	}
 
 	module.initView = function () {
 	
 		this.output = $('#output');
 		$('#calibrate').on('click', function(){
-			document.getElementById('point1').style.visibility = 'visible';
+			$('#point1')[0].style.visibility = 'visible';
 			$('#calibrate').on('click', module.calibrate1);
 		});
+	};
+	
+	module.translateToScreen = function(point){
+		var direction = point.minus(this.screen.origin);
+		var y = this.screen.yu.dot(direction)/this.screen.yscale;
+		var x = this.screen.xu.dot(direction)/this.screen.xscale;
+		return {x: x, y: y};
 	};
 
 	module.initPlane = function() {
 		
-		this.plane = new Plane(this.normals[0],this.normals[1],this.normals[2]);
+		this.plane = new Plane(this.points[0],this.points[1],this.points[2]);
+		
+		this.screen = {};
+		this.screen.center = this.points[0].plus(this.points[2]).dividedBy(2);
+		this.screen.origin = this.points[1].plus(this.points[1].minus(this.screen.center));
+		var yv = this.points[0].minus(this.points[1]);
+		var xv = this.points[2].minus(this.points[0]);
+		this.screen.yu = yv.normalized();
+		this.screen.xu = xv.normalized();
+		this.screen.yscale = 4*yv.magnitude()/document.height;
+		this.screen.xscale = 2*xv.magnitude()/document.width;
 		
 		this.listener.onFrame = function(controller){
 			var pointableList = controller.frame().pointables();
@@ -75,16 +101,22 @@ var Touchscreen = (function () {
 	
 				if (hit && hit.distance < 10) {
 					isHit = true;
+					
+					var screenHit = module.translateToScreen(hit.position);
+					var el = document.createElement("div");
+					el.classList.add("point");
+					el.style.cssText = "left:"+screenHit.x+"px;top:"+screenHit.y+"px;";
+					document.body.appendChild(el);
 				}
 			}
 			
 			//console.log(isHit);
 		
-			if (isHit) {
-				$('body').css('background-color','#ccc');
-			} else {
-				$('body').css('background-color','#fff');
-			}
+			//if (isHit) {
+			//	$('body').css('background-color','#ccc');
+			//} else {
+			//	$('body').css('background-color','#fff');
+			//}
 		};
 		
 	};
